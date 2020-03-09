0f3e9a3a87e5d25c48655fffabf2fea422fb8205
tearill <1105317666@qq.com>
2020-03-09 22:57:14 +0800

    测试组件

--- Source-code/element-ui-code/src/App.vue
+++ Source-code/element-ui-code/src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="app">
-    <el-alert 
+    <!-- <el-alert 
     title="提示信息文字"
     type="error"
     effect="dark"
     show-icon
     closeText='close'
     @close="showMsg"
-    >
+    > -->
       <!-- <template v-slot:title>
         <button>title</button>
         <button>sub-title</button>
       </template> -->
       <!-- <button slot="title">title</button> -->
       <!-- description -->
-      123
+      <!-- 123
     </el-alert>
     <el-alert
     title="成功提示的文案"
@@ -29,17 +29,25 @@
     show-icon
     
     description="这是一句绕口令：黑灰化肥会挥发发灰黑化肥挥发；灰黑化肥会挥发发黑灰化肥发挥。 黑灰化肥会挥发发灰黑化肥黑灰挥发化为灰……">
-    </el-alert>
+    </el-alert> -->
+
+    <carousel height="150px">
+      <carousel-item v-for="item in 4" :key="item">
+        <h3 class="small">{{ item }}</h3>
+      </carousel-item>
+    </carousel>
+    
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import ElAlert from './components/ElAlert.vue'
+// import ElAlert from './components/ElAlert.vue'
 export default {
   name: 'App',
   components: {
-    ElAlert
+    // ElAlert
+    // 'carousel': Carousel
   },
   methods: {
     showMsg() {
@@ -50,5 +58,11 @@ export default {
 </script>
 
 <style>
-
+* {
+  margin: 0;
+  padding: 0;
+}
+html, body {
+  background-color: green;
+}
 </style>
