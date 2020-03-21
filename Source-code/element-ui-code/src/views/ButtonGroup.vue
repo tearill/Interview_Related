722e1e22a1d7a4c327d2bffeb916035345db9a03
tearill <1105317666@qq.com>
2020-03-21 22:18:02 +0800

    button-group展示

---
+++ Source-code/element-ui-code/src/views/ButtonGroup.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <el-button-group>
+      <el-button type="primary" icon="el-icon-arrow-left">上一页</el-button>
+      <el-button type="primary">下一页<i class="el-icon-arrow-right el-icon--right"></i></el-button>
+    </el-button-group>
+    <hr />
+    <el-button-group>
+      <el-button type="primary" icon="el-icon-edit"></el-button>
+      <el-button type="primary" icon="el-icon-share"></el-button>
+      <el-button type="primary" icon="el-icon-delete"></el-button>
+    </el-button-group>
+  </div>
+</template>
+
+<script>
+import ElButtonGroup from '../components/ElButtonGroup.vue'
+import ElButton from '../components/ElButton.vue'
+
+export default {
+  name: 'el-button-group--show',
+  components: {
+    ElButtonGroup,
+    ElButton
+  }
+}
+</script>
+
+<style>
+
+</style>
