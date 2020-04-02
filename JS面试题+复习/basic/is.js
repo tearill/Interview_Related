1b2a960d2b470cb1e2b7a723aaa30cc2d1484e21
tearill <1105317666@qq.com>
2020-04-02 23:32:18 +0800

    Object.is

--- JS面试题+复习/basic/is.js
+++ JS面试题+复习/basic/is.js
@@ -1,6 +1,6 @@
 // Object.is 源码
 function is(x, y) {
-  // 运行到 1/x === 1 的时候 x 和 y 都为0，但是 1/+0 = infinity 1/-0 = -infinity 
+  // 运行到 1/x === 1 的时候 x 和 y 都为0，但是 1/+0 = infinity 1/-0 = -infinity
   if (x === y) {
     return x !== 0 || y !== 0 || 1 / x === 1 / y;
   } else {
