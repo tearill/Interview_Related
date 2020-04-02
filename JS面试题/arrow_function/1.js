3a86ff212cd018e3158c6dc5261ccccc3c0e656d
tearill <1105317666@qq.com>
2020-04-02 17:29:33 +0800

    完善

--- JS面试题/arrow_function/1.js
+++ JS面试题/arrow_function/1.js
@@ -2,11 +2,11 @@
 // ES5版本
 var New = function(fn, ...args) {
   // ES6 的写法 Obeject.setPrototype
-  const obj = Object.create(fn.prototype);
+  const obj = Object.create(fn.prototype); // 直接指定原型
   // var obj = {}; // 最原始的状态，最顶部是一个对象
   // obj.__proto__ = fn.prototype; // 链接原型 对象的__proto__指向构造函数的 prototype
-  fn.call(obj, ...args); // 调用构造初始化
-  return obj;
+  const result = fn.call(obj, ...args); // 调用构造初始化
+  return typeof result === 'object' ? result : obj; // 确保 new 出来的是一个对象
 }
 
 function Func1(name) {
