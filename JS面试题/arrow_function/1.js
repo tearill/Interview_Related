dac8840032559ebab2f975e33fec95dc38a1b78d
tearill <1105317666@qq.com>
2020-04-02 17:10:39 +0800

    手写new的两种方法

---
+++ JS面试题/arrow_function/1.js
@@ -0,0 +1,17 @@
+// 手写一个 new  两个版本 ES6 ES5
+// ES5版本
+var New = function(fn, ...args) {
+  // ES6 的写法 Obeject.setPrototype
+  const obj = Object.create(fn.prototype);
+  // var obj = {}; // 最原始的状态，最顶部是一个对象
+  // obj.__proto__ = fn.prototype; // 链接原型 对象的__proto__指向构造函数的 prototype
+  fn.call(obj, ...args); // 调用构造初始化
+  return obj;
+}
+
+function Func1(name) {
+  this.name = name;
+}
+
+var f1 = New(Func1);
+console.log(f1 instanceof Func1);
