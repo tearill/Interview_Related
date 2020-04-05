bd0b2c7c6ea234c6b7c84927497cba8701692dd3
tearill <1105317666@qq.com>
2020-04-05 21:26:36 +0800

    简易版模板编译

---
+++ Source-code/simple-template-translate/index.js
@@ -0,0 +1,21 @@
+let template = '我是{{name}}, 年龄:{{age}}, 性别:{{sex}}';
+
+let data = {
+  name: "Horace",
+  age: 20,
+  sex: "male"
+}
+
+function render(template, data) {
+  const reg = /\{\{(\w+)\}\}/;
+  // console.log(reg.test(template));
+  if (reg.test(template)) { // 提前预判是否能匹配到
+    const key = reg.exec(template)[1];
+    console.log(key); // 每一个要替换的位置上 {{ }} 中间的内容
+    template = template.replace(reg, data[key]); // 把 {{}} 中间的内容替换成真实数据
+    return render(template, data); // 递归执行去编译下一个 {{}}
+  }
+  return template;
+}
+
+console.log(render(template, data));
