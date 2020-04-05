e32a1c3725d0a2a31512346b538b767261924648
tearill <1105317666@qq.com>
2020-04-05 20:12:28 +0800

    完善版本instanceof

--- JS面试题+复习/basic/instanceof.js
+++ JS面试题+复习/basic/instanceof.js
@@ -11,9 +11,27 @@ function myInstanceof (left, right) {
     proto = Object.getPrototypeOf(proto); // 继续向上查找原型链
   }
 }
+console.log(myInstanceof('111', String)); // false
+console.log(myInstanceof(new String('111'), String)); // true
+console.log(myInstanceof('string', Object)); // false
+console.log(myInstanceof(() => {}, Function), 'function'); // 无法判断！！！！缺陷
 
-console.log(myInstanceof('111', String));
-console.log(myInstanceof(new String('111'), String));
+// instanceof 主要的实现原理就是只要右边变量的 prototype 在左边变量的原型链上
+// instanceof 在查找的过程中会遍历左边变量的原型链，直到找到右边变量的 prototype，如果查找失败，则会返回 false
+function new_instance_of(leftVaule, rightVaule) {
+  let rightProto = rightVaule.prototype; // 取右表达式的 prototype 值
+  leftVaule = leftVaule.__proto__; // 取左表达式的__proto__值
+  while (true) {
+    if (leftVaule === null) { // 遍历完原型链还是没找到
+        return false;	
+      }
+    if (leftVaule === rightProto) { // 在左边变量的原型链上找到了
+      return true;	
+    } 
+    leftVaule = leftVaule.__proto__; // 继续向上查找原型链
+  }
+}
+console.log(new_instance_of(() => {}, Function)); // true
 
 class PrimitiveNumber {
   static [Symbol.hasInstance](x) {
