e0bf03705b3edfb1ace1b57029c63b65e5e25021
tearill <1105317666@qq.com>
2020-04-07 00:04:00 +0800

    call的ES6实现

---
+++ JS面试题+复习/basic/this/call-es6.js
@@ -0,0 +1,37 @@
+Function.prototype.myCall = function(context, ...args) {
+  var context = context || window; // 传递的 this 指向为 null 的时候指向 window
+  // let fn = new Symbol('fn');
+  context.fn = this; // 获取调用 call 的函数，在那个 conext 对象上去添加方法
+  let result = eval('context.fn(...args)');
+  delete context.fn;
+  return result;
+}
+
+let foo = {
+  value: 1
+};
+
+function bar(name, age) {
+  console.log(name);
+  console.log(age);
+  console.log(this.value);
+}
+bar.myCall(foo); // 1
+bar.myCall(foo, 'Horace', 18); // Horace 18 1
+
+var value = 2;
+let obj = {
+  value: 1
+}
+
+function baz(name, age) {
+  console.log(this.value);
+  return {
+    value: this.value,
+    name: name,
+    age: age
+  };
+}
+
+baz.myCall(null);
+console.log(baz.myCall(obj, 'Horace', 18));
