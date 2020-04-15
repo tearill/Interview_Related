c9679f1175bc296ce688c075af7dd322ceb42ccc
tearill <1105317666@qq.com>
2020-04-15 22:08:02 +0800

    链表-数组去重

---
+++ JS面试题+复习/unique_array/linkedList.js
@@ -0,0 +1,36 @@
+// 去重
+// 使用链表
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+const n1 = new ListNode(1);
+const n2 = new ListNode(2);
+const n3 = new ListNode(3);
+const n4 = new ListNode(3);
+const n5 = new ListNode(4);
+const n6 = new ListNode(4);
+n1.next = n2;
+n2.next = n3;
+n3.next = n4;
+n4.next = n5;
+n5.next = n6;
+
+console.log(n1, '-----origin');
+
+function unique(head) {
+  if (head == null || !head.next) return head;
+  let cur = head;
+  while(cur.next) {
+    if (cur.val === cur.next.val) {
+      cur.next = cur.next.next;
+    } else {
+      cur = cur.next;
+    }
+  }
+  return head;
+}
+
+console.log(unique(n1));
+console.log(unique(n1.next));
