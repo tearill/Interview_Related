73a9d73918abcc08487f1d1f60194f0b6bacd9be
tearill <1105317666@qq.com>
2020-04-30 23:09:01 +0800

    new实现

--- JS面试题+复习/basic/this/new.js
+++ JS面试题+复习/basic/this/new.js
@@ -1,10 +1,10 @@
-function myNew() {
-  let obj = {}; // 生成一个新对象
-  const Constructor = [].shift.call(arguments); // 拿到构造函数
+function myNew(obj, ...args) {
+  let newObj = {}; // 生成一个新对象
+  // const Constructor = [].shift.call(arguments); // 拿到构造函数
   // 对象的 __proto__ = 函数的 prototype
-  obj.__proto__ = Constructor.prototype; // 链接原型
-  const ret = Constructor.apply(obj, arguments); // 绑定构造函数的 this 到新创建的对象
-  return typeof ret === 'object' ? ret : obj;
+  newObj.__proto__ = obj.prototype; // 链接原型
+  const ret = obj.apply(newObj, args); // 绑定构造函数的 this 到新创建的对象
+  return typeof ret === 'object' ? ret : newObj;
 }
 
 function Foo(e) {
--- JS面试题+复习/basic/this/new2.js
+++ JS面试题+复习/basic/this/new2.js
@@ -1,10 +1,13 @@
 function myNew(obj, ...args) {
-  let newObj = {}; // 生成一个新对象
+  // let newObj = {}; // 生成一个新对象
   // const Constructor = [].shift.call(arguments); // 拿到构造函数
   // 对象的 __proto__ = 函数的 prototype
   // newObj.__proto__ = obj.prototype; // 链接原型
+  if (typeof obj !== 'function') {
+    throw new Error('new function, the first parameter must be a function')
+  }
   let newObj = Object.create(obj.prototype);
-  const ret = obj.apply(newObj, arguments); // 绑定构造函数的 this 到新创建的对象
+  const ret = obj.apply(newObj, args); // 绑定构造函数的 this 到新创建的对象
   return typeof ret === 'object' ? ret : obj;
 }
 
@@ -13,4 +16,5 @@ function Foo(e) {
   this.age = e;
 }
 
-console.log(myNew(Foo, 18));
+let newFoo = myNew(Foo, 18)
+console.log(newFoo);
